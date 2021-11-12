bff4fb8093d533f4924e6f370a76e9687e4571fc
Cecilia Lorenzini <lcecilia8147@gmail.com>
2021-11-12 18:02:15 -0300

    Menu
    Se agregó el menu correspondiente a la parte 1 del tp

--- TP2/App.java
+++ TP2/App.java
@@ -9,21 +9,84 @@ public class App {
      */
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        RegistrarMateria M1 = new RegistrarMateria("PBA", "Progamacion Basica.");
         
-        List <Alumno> MatricularAlumno = new ArrayList();
+        Menu();
         
-        M1.toString();
-        MatricularAlumno.add( new Alumno(in.nextInt(), in.nextLine(),in.nextLine(),in.nextInt(),in.nextLine()));
-        int nLegajoE = 0;
-        for (int i = 0; i < MatricularAlumno.size(); i++) {
-            if(MatricularAlumno.getnLegajo(i) == nLegajoE ){
-                MatricularAlumno.remove(MatricularAlumno.get(i));
-                System.out.println("El Alumno ha sido borrado exitosamente");
-            } else {
-                System.out.println("El Alumno no se encuentra en el sistema");
-            }
+       
+        
+       
+       // MatricularAlumno.add( new Alumno(in.nextInt(), in.nextLine(),in.nextLine(),in.nextInt(),in.nextLine()));
+
+        
+    }
+    public static void Menu(){
+        Scanner in = new Scanner(System.in);
+        System.out.println("    MENÚ INICIO ");
+        System.out.println(" ==================== ");
+        System.out.println("1. -- Registrar Materia");
+        System.out.println("9.- Salir. ");
+        System.out.println("Ingrese una opción");
+        
+        int opcion = in.nextInt();
+        switch (opcion) {
+            case 1:
+                RegistrarMateria M1 = new RegistrarMateria(in.nextLine(), in.nextLine());
+                System.out.println(M1);
+                // Menu gestionar materia
+                MenuGestionaMateria();
+                break;
+            case 9:
+                System.out.println("Gracias. Uested ha salido del sistema");
+                break;
         }
+
+    }
+    public static void MenuGestionaMateria(){
+        Scanner in = new Scanner(System.in);
+        System.out.println("    GESTIONAR MATERIA   ");
+        System.out.println(" =========================== ");
+        System.out.println("1.- Matricular Alumno");
+        System.out.println("2.- Desmatricular Alumno");
+        System.out.println("3.- Gestionar Asistencia ");
+        System.out.println("-- Consultas. ");
+        System.out.println("        4.-  Listado de inscriptos ");
+        System.out.println("        5.-  Visualizar Asistencia ");
+        System.out.println("9.- Salir");
+        System.out.println("Ingrese una opcion ");
+        int opcion = in.nextInt();
+        switch(opcion){
+            case 1: System.out.println("MATRICULAR ALUMNO");
+                    MatricularAlumno();
+            break;
+            case 2: System.out.println("DESMATRICULAR ALUMNO");
+            break;
+            case 3: System.out.println("GESTIONAR ASISTENCIA");
+            break;
+            case 4: System.out.println("LISTADO DE INSCRIPTOS");
+            break;
+            case 5: System.out.println("VISULIZAR ASISTENCIA");
+        
+        }
+        
+    }
+    public static void MatricularAlumno(){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Ingrese al nuevo alumno");
+         List <Alumno> MatricularAlumno = new ArrayList();
+         //me pide los datos del alumno por consola
+         System.out.println("Ingresa el Número de legajo : ");
+         int nLegajo = in.nextInt();
+          System.out.println("Ingresa el Nombre : ");
+         String nombre = in.nextLine(); 
+         System.out.println("Ingresa el Apellido : ");
+         String apellido = in.nextLine();
+         System.out.println("Ingresa la fecha de nacimiento : ");
+         int fecha = in.nextInt();
+         System.out.println("Ingresa el Email : ");
+         String email = in.nextLine();
+         //me  guarda los datos en el array
+         MatricularAlumno.add(new Alumno(nLegajo,nombre,apellido, fecha,email));
+         System.out.println(MatricularAlumno);
     }
     
 }
--- TP2/GetionarMateria.java
+++ TP2/GetionarMateria.java
@@ -19,6 +19,59 @@ public class GetionarMateria {
         this.fechaNacimiento = fechaNacimiento;
         this.email = email;
     }
+
+    public int getnLegajo() {
+        return nLegajo;
+    }
+
+    public void setnLegajo(int nLegajo) {
+        this.nLegajo = nLegajo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(int fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getFechaDeHoy() {
+        return fechaDeHoy;
+    }
+
+    public void setFechaDeHoy(int fechaDeHoy) {
+        this.fechaDeHoy = fechaDeHoy;
+    }
+
+    @Override
+    public String toString() {
+        return "GetionarMateria{" + "nLegajo=" + nLegajo + ", nombre=" + nombre + ", apellido=" + apellido + ", fechaNacimiento=" + fechaNacimiento + ", email=" + email + ", fechaDeHoy=" + fechaDeHoy + '}';
+    }
     
     
 }
